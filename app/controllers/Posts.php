67af73f3481b627c7c276e97c9012c4ef7abdcca
Maarja Tapsi <maarja.tapsi@khk.ee>
2020-01-16 09:05:48 +0200

    Kõik üles

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -10,6 +10,6 @@ class Posts
      */
     public function __construct()
     {
-
+        echo 'Posts class loaded';
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
 require_once '../app/bootstrap.php';
+// init app core
+$init = new Core();
+echo '<pre>';
+    print_r($init);
+echo '</pre>';
